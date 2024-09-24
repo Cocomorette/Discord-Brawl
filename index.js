281b3c7289e00f715c4d649e61d5f72dc5782f67
Cocomorette <sincronizartodo23@gmail.com>
2024-09-24 11:50:21 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { Client, Events, GatewayIntentBits, REST, Routes } = require("discord.js");
-const keep alive = require('./keep_alive.js')
+const keep_alive = require('./keep_alive.js')
 const axios = require('axios');
 
 // Reemplaza con tu clave API y el ID del jugador
